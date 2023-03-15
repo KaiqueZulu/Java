fce7a1adafdc2f70205a24b5cc05ffd0838eb2ee
KaiqueZulu <kaiqueroberto.pires@gmail.com>
2023-03-15 02:48:04 +0000

    Vectors and matrices

--- vectorsAndMatrices/src/vectors.java
+++ vectorsAndMatrices/src/vectors.java
@@ -1,18 +1,18 @@
 public class vectors {
     public static void main(String[] args) throws Exception {
-        //Declaring the array
+        // Declaring the array
         int vectors[];
 
-        //Declaring and specifying the array size
+        // Declaring and specifying the array size
         int numbers[] = new int[5];
         float floats[] = new float[5];
         char chars[] = new char[5];
         String strings[] = new String[5];
 
-        //Declaring and start the array
-        int others_numbers[] = {1, 3, 5, 7, 9};
+        // Declaring and start the array
+        int others_numbers[] = { 1, 3, 5, 7, 9 };
 
         System.out.println("Hello, World!");
-    
+
     }
 }
